708481ad631f20fcd9044b1dafe98d51eb5d7dd4
Ricardo Rosinha <rrosinha@live.com>
2020-03-15 18:57:58 +0000

    FirstUpload

---
+++ simplePegDraw.py
@@ -0,0 +1,29 @@
+# simplepegdraw.py
+""" Draw fishing pegs based on a given number of fishermen. 
+
+    Author: Ricardo Rosinha
+"""
+
+import sys
+import random
+
+def number_of_fishers():
+    """ Prompt the user for the number of fishermen entering the draw."""
+    try:
+        number = int(input("How many fishermen will enter the competition: "))
+        return number
+    except ValueError:
+        print("Please enter an integer for the number of competing fishermen")
+    
+
+def draw_and_print(fnumber):
+    """ Draw and print the pegs for a given number of fishermen. """
+    pegs = [i for i in range(1, fnumber+1)]
+    print(pegs)
+    random.shuffle(pegs)
+    print(pegs)
+
+
+
+if __name__ == "__main__":
+    number_of_fishers()
